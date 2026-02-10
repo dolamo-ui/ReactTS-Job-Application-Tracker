7d19808985a4354e9832d305ad7e01af997b7644
Nyakallo Dolamo <susandolamo@gmail.com>
2026-02-10 15:19:19 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-// File: src/App.tsx
+
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import LandingPage from "./pages/LandingPage";
 import AuthPage from "./pages/AuthPage";
 import NotFoundPage from "./pages/NotFoundPage";
 
-// Protected Route Component
+
 const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
   const userId = localStorage.getItem('userId');
   
@@ -20,12 +20,12 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* Public Pages */}
+       
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<AuthPage mode="login" />} />
         <Route path="/register" element={<AuthPage mode="register" />} />
 
-        {/* Protected Pages */}
+       
         <Route
           path="/home"
           element={
@@ -35,11 +35,12 @@ function App() {
           }
         />
 
-        {/* Catch-All Route for 404 */}
+       
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Router>
   );
 }
+
 
 export default App;
